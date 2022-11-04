deedb0779d269d6386041ce3e11478ecc76e8af7
Megan Pretorius <10207@ctucareer.co.za>
2022-11-04 10:45:15 +0200

    ASP.NET Project 1

---
+++ SLD521 Fa2/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SLD521_Fa2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Location()
+        {
+            ViewBag.Message = "Your location page.";
+
+            return View();
+        }
+
+        public ActionResult Favorites()
+        {
+            ViewBag.Message = "Your favorites page.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
+
+        //    return View();
+        //}
+
+        
+
+        public ActionResult News()
+        {
+            ViewBag.Message = "Your news page.";
+
+            return Redirect("https://screenrant.com/comics/");
+        }
+
+        
+    }
+}
---
+++ SLD521 Fa2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SLD521_Fa2.MvcApplication" Language="C#" %>
